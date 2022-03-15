8b28659e1a46791d757a9fb009076ad66e5b5829
Kawthar Al-Tamimi <kawthermujahed@gmail.com>
2022-03-15 14:07:23 +0300

    Solve 2 warm-up challenges

---
+++ interview preparation kit/WarmUp/countingValleys.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+func countingValleys(steps: Int, path: String) -> Int {
+
+    var valleysNumber = 0 
+    var altitude = 0
+    
+    for index in 0..<path.count {
+        var ch = Array(path)[index]
+        if( ch == "U") {
+            altitude += 1
+            if (altitude == 0) {
+                valleysNumber += 1
+            }
+        }
+        else {
+            altitude -= 1
+        }
+    }  
+    return valleysNumber
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let steps = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let path = readLine() else { fatalError("Bad input") }
+
+let result = countingValleys(steps: steps, path: path)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
---
+++ interview preparation kit/WarmUp/sockMerchant.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+func sockMerchant(n: Int, ar: [Int]) -> Int {
+    // Write your code here
+    var frequencyArr : [Int] = [Int]()
+    var match : Int = 0
+    
+    for i in 0..<n {
+        let currentColor = ar[i]
+        if let index = frequencyArr.index(of: currentColor)  {
+            match = match + 1
+            frequencyArr.remove(at: index)
+        } else {
+             frequencyArr.append(currentColor)
+        }
+    }
+    
+    return match
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let ar: [Int] = arTemp.split(separator: " ").map {
+    if let arItem = Int($0) {
+        return arItem
+    } else { fatalError("Bad input") }
+}
+
+guard ar.count == n else { fatalError("Bad input") }
+
+let result = sockMerchant(n: n, ar: ar)
+
+fileHandle.write(String(result).data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
