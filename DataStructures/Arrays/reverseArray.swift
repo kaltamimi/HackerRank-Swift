5015847befaafa1599f4dd7cffa869860a165b76
Kawthar Al-Tamimi <kawthermujahed@gmail.com>
2022-03-28 20:08:20 +0300

    reverseArray solution

---
+++ DataStructures/Arrays/reverseArray.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+func reverseArray(a: [Int]) -> [Int] {
+    // Write your code here
+    var reversed:[Int] = a.reversed()
+    return reversed
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let arrCount = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == arrCount else { fatalError("Bad input") }
+
+let res = reverseArray(a: arr)
+
+fileHandle.write(res.map{ String($0) }.joined(separator: " ").data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
