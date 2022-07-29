afe10075b79565d77fd15d551f9bdfeb1c96406b
Kawthar Al-Tamimi <kawthermujahed@gmail.com>
2022-07-29 22:49:14 +0300

    left rotation solution

---
+++ DataStructures/Arrays/leftRotation.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+/*
+ * Complete the 'rotateLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER d
+ *  2. INTEGER_ARRAY arr
+ */
+
+func rotateLeft(d: Int, arr: [Int]) -> [Int] {
+    // Write your code here
+
+    let slice1 = arr[..<d]
+    let slice2 = arr[d...]
+    return Array(slice2) + Array(slice1)
+}
+
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let firstMultipleInputTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+let firstMultipleInput = firstMultipleInputTemp.split(separator: " ").map{ String($0) }
+
+guard let n = Int(firstMultipleInput[0])
+else { fatalError("Bad input") }
+
+guard let d = Int(firstMultipleInput[1])
+else { fatalError("Bad input") }
+
+guard let arrTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+let result = rotateLeft(d: d, arr: arr)
+
+fileHandle.write(result.map{ String($0) }.joined(separator: " ").data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
